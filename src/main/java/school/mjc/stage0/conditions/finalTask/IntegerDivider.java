8285622bed9e773b06ec358b2aaaf577b454cbbf
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-16 12:20:06 -0400

    Update IntegerDivider.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        float  newValue =dividend/divider;
+         float newNUmber = newValue * divider;
+
+        if (newNUmber==dividend){
+            System.out.println("can be divided completely");
+        }  else{
+            System.out.println("cannot be divided completely");
+        }
     }
 }
